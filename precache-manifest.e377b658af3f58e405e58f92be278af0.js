d57f6e4c6e04e21c03cc666b8509a67e0a8d09eb
jennlindley <jennifersu2059@gmail.com>
2020-04-20 18:09:29 -0500

    deploy

---
+++ precache-manifest.e377b658af3f58e405e58f92be278af0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e432bb1917beb03062",
+    "url": "/RedSocial2.0/css/app.fd62fc22.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/RedSocial2.0/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3c2c45ede6dd29488d1448bc1bcaacf9",
+    "url": "/RedSocial2.0/index.html"
+  },
+  {
+    "revision": "354416487c4e7106fecc",
+    "url": "/RedSocial2.0/js/about.541f07e6.js"
+  },
+  {
+    "revision": "36e432bb1917beb03062",
+    "url": "/RedSocial2.0/js/app.59cdb347.js"
+  },
+  {
+    "revision": "45e13053ec1f58887475",
+    "url": "/RedSocial2.0/js/chunk-vendors.ade13aea.js"
+  },
+  {
+    "revision": "304d712cf463a5dc1a9608d993ecb10f",
+    "url": "/RedSocial2.0/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/RedSocial2.0/robots.txt"
+  }
+]);
